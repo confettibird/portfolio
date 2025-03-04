b5fee7ab10f3cfbd99df4b38c17607d334b45da1
Noelyn Stephens <hello@noelynstephens.com>
2025-03-04 15:46:06 -0600

    update menu transitions

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,52 +1,47 @@
 <template>
-  <div id="navigation">
-    <div @click="toggleMenu()" id="menu-icon" :class="{ open: displayMenu }">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <transition
-      name="menu"
-      enter-active-class="animated slideInRight"
-      leave-active-class="animated slideOutRight"
-    >
-      <div class="menu-content" v-if="displayMenu">
-        <div class="menu-slope"></div>
-        <div class="menu-center menu-wrapper">
-          <ul class="menu-items" @click="toggleMenu">
-            <li><a class="navItem" href="#about" v-smooth-scroll>About</a></li>
-            <li>
-              <a class="navItem" href="#skills" v-smooth-scroll>Skills</a>
-            </li>
-            <li>
-              <a class="navItem" href="#projects" v-smooth-scroll
-                >Featured<span style="color: rgba(255, 255, 255, 0)">.</span
-                >Projects</a
-              >
-            </li>
-            <li>
-              <a class="navItem" href="#contact" v-smooth-scroll>Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </transition>
-  </div>
+	<div id="navigation">
+		<div @click="toggleMenu()" id="menu-icon" :class="{ open: displayMenu }">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<transition name="menu">
+			<div class="menu-content" v-show="displayMenu">
+				<div class="menu-center menu-wrapper">
+					<ul class="menu-items">
+						<li><a class="navItem" href="#about" v-smooth-scroll>About</a></li>
+						<li>
+							<a class="navItem" href="#skills" v-smooth-scroll>Skills</a>
+						</li>
+						<li>
+							<a class="navItem" href="#projects" v-smooth-scroll
+								>Featured<span style="color: rgba(255, 255, 255, 0)">.</span>Projects</a
+							>
+						</li>
+						<li>
+							<a class="navItem" href="#contact" v-smooth-scroll>Contact</a>
+						</li>
+					</ul>
+				</div>
+				<div class="menu-slope"></div>
+			</div>
+		</transition>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      displayMenu: false
-    };
-  },
-  methods: {
-    toggleMenu: function() {
-      this.displayMenu = !this.displayMenu;
-    }
-  }
+	data() {
+		return {
+			displayMenu: false,
+		};
+	},
+	methods: {
+		toggleMenu: function () {
+			this.displayMenu = !this.displayMenu;
+		},
+	},
 };
 </script>
 
@@ -54,62 +49,85 @@ export default {
 @import "@/scss/widgets/hamburger.scss";
 
 .menu-content {
-  position: fixed;
-  right: 0;
-  top: 0;
-  z-index: 100;
-  width: 30%;
-  height: 100vh;
-  background-color: #4db4d7;
-  @media (max-width: $lrg-screen) {
-    width: 100%;
-  }
+	position: fixed;
+	right: 0;
+	top: 0;
+	z-index: 100;
+	width: 30%;
+	height: 100vh;
+	background-color: #4db4d7;
+	@media (max-width: $lrg-screen) {
+		width: 100%;
+	}
 }
 
 .menu-wrapper {
-  border-left: 2px solid #fdf3c8;
-  padding: 5%;
+	border-left: 2px solid #fdf3c8;
+	padding: 5%;
 }
 
 .menu-slope {
-  @include slope(#4db4d7, #0076be, up);
-  width: 100%;
-  height: 75px;
-  position: absolute;
-  bottom: 0;
+	@include slope(#4db4d7, #0076be, up);
+	width: 100%;
+	height: 75px;
+	position: absolute;
+	bottom: 0;
 }
 
 .menu-center {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  @media (max-width: $md-screen) {
-    top: 40%;
-  }
-  @media (max-width: $sm-screen) {
-    top: 30%;
-  }
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	-webkit-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+	@media (max-width: $md-screen) {
+		top: 40%;
+	}
+	@media (max-width: $sm-screen) {
+		top: 30%;
+	}
+}
+.menu-enter-active {
+	transition: transform 0.1s ease-in;
+}
+
+.menu-leave-active {
+	transition: transform 0.3s ease-out;
+}
+
+.menu-enter {
+	transform: translateX(100%);
+}
+
+.menu-enter-to {
+	transform: translateX(0);
+}
+
+.menu-leave-from {
+	transform: translateX(0);
+}
+
+.menu-leave-to {
+	transform: translateX(100%);
 }
 
 .menu-items {
-  font: normal 28px $font-section-title;
-  li {
-    display: block;
-    list-style: none;
-    padding-bottom: 10%;
-  }
-  a {
-    color: $color-light-2;
-  }
-  a:active,
-  a:link {
-    color: $color-light-2;
-    text-decoration: none;
-  }
-  a:hover {
-    color: #f1c245;
-  }
+	font: normal 28px $font-section-title;
+	li {
+		display: block;
+		list-style: none;
+		padding-bottom: 10%;
+	}
+	a {
+		color: $color-light-2;
+	}
+	a:active,
+	a:link {
+		color: $color-light-2;
+		text-decoration: none;
+	}
+	a:hover {
+		color: #f1c245;
+	}
 }
 </style>
--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,95 +1,107 @@
 <template>
-  <div>
-    <section id="skills" class="section--2">
-      <div class="section__body">
-        <div class="section__title">
-          <h3 style="font-size:36px">Technical Skills</h3>
-          <!-- <span class="box-1"> </span> -->
-        </div>
-        <div class="centered skill-grid">
-          <div id="wordcloud">
-            <vue-word-cloud
-              :words="[
-                ['three.js', 1.5],
-                ['JavaScript', 2],
-                ['Vue.js', 2],
-                ['WordPress', 2],
-                ['Photoshop', 2.25],
-                ['CSS/Sass', 2.25],
-                ['Figma', 1.5],
-                ['Illustrator', 2.25],
-                ['InDesign', 1.5]
-              ]"
-              :color="
-                ([, weight]) =>
-                  weight > 3
-                    ? '#023183'
-                    : weight > 2
-                    ? '#4276CF'
-                    : weight > 1
-                    ? '#056299'
-                    : '#04808F'
-              "
-              :spacing="0.5"
-              font-family="Jost, sans-serif"
-            />
-          </div>
-        </div>
-      </div>
-      <!-- <div class="section__title section__title--right">Skills</div> -->
-    </section>
-  </div>
+	<div>
+		<section id="skills" class="section--2">
+			<div class="section__body">
+				<div class="section__title">
+					<h3 style="font-size: 36px">Skills</h3>
+					<!-- <span class="box-1"> </span> -->
+				</div>
+				<div class="centered skill-grid" style="font-family:Jost, sans-serif">
+          <!-- new skills layout -->
+					<div class="skill-cols">
+						<div class="col-sk-1">
+							<h3 style="font-size: 36px">Front-end</h3>
+						</div>
+						<div class="col-sk-2">
+							<h3 style="font-size: 36px">Backend</h3>
+						</div>
+						<div class="col-sk-3">
+							<h3 style="font-size: 36px">Team</h3>
+						</div>
+					</div>
+					<div id="wordcloud">
+						<vue-word-cloud
+							:words="[
+								['three.js', 1.5],
+								['JavaScript', 2],
+								['Vue.js', 2],
+								['WordPress', 2],
+								['Photoshop', 2.25],
+								['CSS/Sass', 2.25],
+								['Figma', 1.5],
+								['Illustrator', 2.25],
+								['InDesign', 1.5],
+							]"
+							:color="
+								([, weight]) =>
+									weight > 3
+										? '#023183'
+										: weight > 2
+										? '#4276CF'
+										: weight > 1
+										? '#056299'
+										: '#04808F'
+							"
+							:spacing="0.5"
+							font-family="Jost, sans-serif"
+						/>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="section__title section__title--right">Skills</div> -->
+		</section>
+	</div>
 </template>
 
 <script>
 import vueWordCloud from "vuewordcloud";
 export default {
-  components: {
-    vueWordCloud
-  }
+	components: {
+		vueWordCloud,
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 #skills {
-  .section__body {
-    border-top: 1px solid $color-border;
-    margin: 0 2rem;
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    padding: 5rem 0 10rem;
-  }
-  .section__title {
-    display: flex;
-    justify-content: start;
-    font-size: 3rem;
-    text-align: right;
-    width: 79.5%;
-    margin: 1rem auto 3rem;
-    position: relative;
-    h3 {
-      z-index: 10;
-    }
-    .box-1 {
-      position: absolute;
-      background-color: #64faea;
-      height: 3px;
-      width: 33%;
-      left: 0px;
-      top: 32px;
-    }
-  }
+	.section__body {
+		border-top: 1px solid $color-border;
+		margin: 0 2rem;
+		position: relative;
+		display: flex;
+		flex-direction: row;
+		padding: 5rem 0 10rem;
+	}
+	.section__title {
+		display: flex;
+		justify-content: start;
+		font-size: 3rem;
+		text-align: right;
+		width: 79.5%;
+		margin: 1rem auto 3rem;
+		position: relative;
+		h3 {
+			z-index: 10;
+		}
+		.box-1 {
+			position: absolute;
+			background-color: #64faea;
+			height: 3px;
+			width: 33%;
+			left: 0px;
+			top: 32px;
+		}
+	}
 }
 .skill-grid {
-  width: 100%;
-  height: 70%;
-  // margin: 10rem 15px;
+	width: 100%;
+	height: 70%;
+	// margin: 10rem 15px;
 }
 
 #wordcloud {
-  width: 65%;
-  height: 50vh;
-  margin: auto;
+	width: 65%;
+	height: 50vh;
+	margin: auto;
 }
 </style>
